36844f856f212a6256e4286f8a27a593605204cf
김하서 <gktj0698@gmail.com>
2024-11-24 04:06:26 +0900

    돌앗다

--- controller/driving_contorller.py
+++ controller/driving_contorller.py
@@ -1,17 +1,55 @@
 from solver.trajectory import GaitPatternGenerator
+from solver.inverse import  Kinematics
+from config.config import leg_resolution
+#from controller.motor_controller import MotorController
 
 robot_speed = 30 #mm/s
 robot_step_hight = 30
 robot_motion = "forward"
-robot_oriention = robot_orientation = [0, 10, 0]
+robot_oriention = [0, 0, 0]
 
+inverse_kinematics = Kinematics()
 
 def driving_contoller():
 
     pattern_generator = GaitPatternGenerator()
-    test = pattern_generator.generate_crawl_gait_pattern(robot_motion)
+    inverse_kinematics = Kinematics()
+    foot_poses = pattern_generator.generate_crawl_gait_pattern(robot_speed,robot_step_hight,robot_motion)
+    motor_control(foot_poses)
 
+def motor_control(foot_pose):
 
+    base_foot_poses = inverse_kinematics.calculate_foot_position_with_orientation(*robot_oriention)
+    num_steps = leg_resolution
+    print(foot_pose[0][1][0]+base_foot_poses[0][1])
+    for i in range(10):
+        for i in range(num_steps):
+            fl_theta1, fl_theta2, fl_theta3 = inverse_kinematics.calculate_joint_angle(False, foot_pose[0][0][i]+base_foot_poses[0][0], foot_pose[0][1][i]+base_foot_poses[0][1], foot_pose[0][2][i]+base_foot_poses[0][2])
+            fr_theta1, fr_theta2, fr_theta3 = inverse_kinematics.calculate_joint_angle(True, foot_pose[1][0][i]+base_foot_poses[1][0], foot_pose[1][1][i]+base_foot_poses[1][1], foot_pose[1][2][i]+base_foot_poses[1][2])
+            rl_theta1, rl_theta2, rl_theta3 = inverse_kinematics.calculate_joint_angle(False, foot_pose[2][0][i]+base_foot_poses[2][0], foot_pose[2][1][i]+base_foot_poses[2][1], foot_pose[2][2][i]+base_foot_poses[2][2])
+            rr_theta1, rr_theta2, rr_theta3 = inverse_kinematics.calculate_joint_angle(True, foot_pose[3][0][i]+base_foot_poses[3][0], foot_pose[3][1][i]+base_foot_poses[3][1], foot_pose[3][2][i]+base_foot_poses[3][3])
+
+            print(fl_theta1)
+#             angle_commands = [
+#                 AngleCommand("lf_joint1", -fl_theta2),
+#                 # AngleCommand("lf_joint2", -fl_theta2),
+#                 # AngleCommand("lf_joint3", -2 * (fl_theta3)),
+#                 # AngleCommand("rf_joint1", -fr_theta1),
+#                 AngleCommand("rf_joint2", -2 * (fl_theta3)),
+#                 # AngleCommand("rf_joint3", -2 * (fr_theta3)),
+#                 # AngleCommand("lr_joint1", -rl_theta1),
+#                 # AngleCommand("lr_joint2", -rl_theta2),
+#                 # AngleCommand("lr_joint3", -2 * (rl_theta3)),
+#                 # AngleCommand("rr_joint1", -rr_theta1),
+#                 # AngleCommand("rr_joint2", -rr_theta2),
+#                 # AngleCommand("rr_joint3", -2 *(rr_theta3)),
+#             ]
+#
+#             # 모터 명령 동기적으로 실행
+#             motor_controller.move_motors(angle_commands)
+#             # 약간의 지연 추가
+#             time.sleep(motor_delay)
+#     motor_controller.close()
 
 if __name__ == "__main__":
     driving_contoller()
--- solver/trajectory.py
+++ solver/trajectory.py
@@ -16,7 +16,7 @@ class GaitPatternGenerator:
         # self.motor_delay = 1 / self.hz
 
 
-    def foot_trajectory(self, time, start_step, speed, step_height):
+    def foot_trajectory(self, time, start_step, foot_direction ,speed, step_height):
         """
         한쪽 발의 궤적 계산.
         :param time: 시간 배열
@@ -27,38 +27,118 @@ class GaitPatternGenerator:
         self.step_length = speed / 2
         self.step_height = step_height
 
-        if start_step == "forward" :
-            x = np.where(
-                (0 <= time) & (time < 2 / 2),
-                self.step_length * (-1 + 2 * time / (2 / 2)),
-                self.step_length * (1 - 2 * (time - 2 / 2) / (2 / 2))
-            )
-            y = np.zeros_like(time)
-            z = np.where(
-                (0 <= time) & (time < 2 / 2),
-                self.step_height * np.sin(np.pi * time / (2 / 2)),
-                0
-            )
-        elif start_step == "backward" :
-            x = np.where(
-                (0 <= time) & (time < 2 / 2),
-                self.step_length * (1 - 2 * time / (2 / 2)),
-                self.step_length * (-1 + 2 * (time - 2 / 2) / (2 / 2))
-            )
-            y = np.zeros_like(time)
-            z = np.where(
-                (0 <= time) & (time < 2 / 2),
-                0,
-                self.step_height * np.sin(np.pi * (time - 2 / 2) / (2 / 2))
-            )
+        if start_step == 1 :
 
-        elif start_step == "left" :
+            if foot_direction == "forward":
+                x = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    self.step_length/2 * (-1 + 2 * time / (2 / 2)) + (self.step_length/2),
+                    self.step_length/2 * (1 - 2 * (time - 2 / 2) / (2 / 2)) + (self.step_length/2)
+                )
+                y = np.zeros_like(time)
+                z = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    self.step_height * np.sin(np.pi * time / (2 / 2)),
+                    0
+                )
 
-            pass
+            elif foot_direction == "backward":
+                x = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    -self.step_length/2 * (-1 + 2 * time / (2 / 2)) - (self.step_length/2),
+                    -self.step_length/2 * (1 - 2 * (time - 2 / 2) / (2 / 2)) - (self.step_length/2)
+                )
+                y = np.zeros_like(time)
+                z = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    self.step_height * np.sin(np.pi * time / (2 / 2)),
+                    0
+                )
 
-        elif start_step == "right" :
+            elif foot_direction == "left":
 
-            pass
+                x = np.zeros_like(time)
+                y = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    -self.step_length/2 * (-1 + 2 * time / (2 / 2)) - (self.step_length/2),
+                    -self.step_length/2 * (1 - 2 * (time - 2 / 2) / (2 / 2)) - (self.step_length/2)
+                )
+                z = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    self.step_height * np.sin(np.pi * time / (2 / 2)),
+                    0
+                )
+
+            elif foot_direction == "right":
+
+                x = np.zeros_like(time)
+                y = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    self.step_length/2 * (-1 + 2 * time / (2 / 2)) + (self.step_length/2),
+                    self.step_length/2 * (1 - 2 * (time - 2 / 2) / (2 / 2)) + (self.step_length/2)
+                )
+                z = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    self.step_height * np.sin(np.pi * time / (2 / 2)),
+                    0
+                )
+
+        elif start_step == 0 :
+
+            if foot_direction == "forward":
+                x = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    self.step_length/2 * (1 - 2 * time / (2 / 2)) + (self.step_length/2),
+                    self.step_length/2 * (-1 + 2 * (time - 2 / 2) / (2 / 2)) + (self.step_length/2)
+                )
+                y = np.zeros_like(time)
+                z = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    0,
+                    self.step_height * np.sin(np.pi * (time - 2 / 2) / (2 / 2))
+                )
+
+            if foot_direction == "backward":
+                x = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    -self.step_length/2 * (1 - 2 * time / (2 / 2)) - (self.step_length/2),
+                    -self.step_length/2 * (-1 + 2 * (time - 2 / 2) / (2 / 2)) - (self.step_length/2)
+                )
+                y = np.zeros_like(time)
+                z = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    0,
+                    self.step_height * np.sin(np.pi * (time - 2 / 2) / (2 / 2))
+                )
+
+            if foot_direction == "left":
+
+                x=np.zeros_like(time)
+                y = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    - self.step_length / 2 * (1 - 2 * time / (2 / 2)) - (self.step_length / 2),
+                    - self.step_length / 2 * (-1 + 2 * (time - 2 / 2) / (2 / 2)) - (self.step_length / 2)
+                )
+                z = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    0,
+                    self.step_height * np.sin(np.pi * (time - 2 / 2) / (2 / 2))
+                )
+
+            if foot_direction == "right":
+
+                x=np.zeros_like(time)
+                y = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    self.step_length / 2 * (1 - 2 * time / (2 / 2)) + (self.step_length / 2),
+                    self.step_length / 2 * (-1 + 2 * (time - 2 / 2) / (2 / 2)) + (self.step_length / 2)
+                )
+                z = np.where(
+                    (0 <= time) & (time < 2 / 2),
+                    0,
+                    self.step_height * np.sin(np.pi * (time - 2 / 2) / (2 / 2))
+                )
+
 
         return np.array(x), np.array(y) , np.array(z)
 
@@ -70,22 +150,30 @@ class GaitPatternGenerator:
         time = np.linspace(0, 2, self.resolution)
 
         if robot_motion == "forward":
-            foot_direction = ["forward","backward","backward","forward"]
+            foot_direction = ["forward","forward","forward","forward"]
 
-        fl_x, fl_y, fl_z = self.foot_trajectory(time, foot_direction[0], self.speed , self.step_hight)
-        fr_x, fr_y, fr_z = self.foot_trajectory(time, foot_direction[1], self.speed , self.step_hight)
-        rl_x, rl_y, rl_z = self.foot_trajectory(time, foot_direction[2], self.speed , self.step_hight)
-        rr_x, rr_y, rr_z = self.foot_trajectory(time, foot_direction[3], self.speed , self.step_hight)
+        elif robot_motion == "backward":
+            foot_direction = ["backward","backward","backward","backward"]
+
+        elif robot_motion == "left":
+            foot_direction = ["left","left","left","left"]
+
+        elif robot_motion == "right":
+            foot_direction = ["right","right","right","right"]
+
+        elif robot_motion == "left_turn":
+            foot_direction = ["backward","forward","backward","forward"]
+
+        elif robot_motion == "right_turn":
+            foot_direction = ["forward","backward","forward","backward"]
 
 
-        #
-        #
-        # return (
-        #     x_front_left, z_front_left,
-        #     x_front_right, z_front_right,
-        #     x_rear_left, z_rear_left,
-        #     x_rear_right, z_rear_right
-        # )
+        fl_coords  = self.foot_trajectory(time,1,foot_direction[0], self.speed , self.step_hight)
+        fr_coords  = self.foot_trajectory(time,0,foot_direction[1], self.speed , self.step_hight)
+        rl_coords = self.foot_trajectory(time,0,foot_direction[2], self.speed , self.step_hight)
+        rr_coords  = self.foot_trajectory(time,1,foot_direction[3], self.speed , self.step_hight)
+        return (fl_coords, fr_coords, rl_coords, rr_coords)
+
 
 if __name__ == "__main__":
 
