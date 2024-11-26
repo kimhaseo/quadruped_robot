61427aa652549c4294980aba0237f89d3263e6ae
김하서 <gktj0698@gmail.com>
2024-11-26 23:32:54 +0900

    ffgfj

--- controller/driving_contorller.py
+++ controller/driving_contorller.py
@@ -1,88 +1,125 @@
 from solver.trajectory import GaitPatternGenerator
-from solver.inverse import  Kinematics
+from solver.inverse import Kinematics
 from config.config import leg_resolution
 from config.motor_cmd import AngleCommand
 from controller.motor_controller import MotorController
-from config.config import leg_resolution
 import time
 import math
 
-robot_speed = 120 #mm/s
-distance = 2400 #mm
-robot_step_hight = 50 #mm
-robot_motion = "forward"
-robot_orientaion = [0, 0, 0]
-motor_delay = 1 / (leg_resolution*2)
 
-inverse_kinematics = Kinematics()
-motor_controller = MotorController()
+class RobotController:
+    def __init__(self, robot_speed, distance, step_height, motion, orientation):
+        self.robot_speed = robot_speed  # mm/s
+        self.distance = distance  # mm
+        self.step_height = step_height  # mm
+        self.motion = motion
+        self.orientation = orientation
 
-def driving_contoller():
+        self.inverse_kinematics = Kinematics()
+        self.motor_controller = MotorController()
+        self.pattern_generator = GaitPatternGenerator()
 
-    pattern_generator = GaitPatternGenerator()
+    def calculate_steps(self):
+        step_count = self.distance / self.robot_speed
+        return step_count
 
-    step_count = distance / robot_speed
-    print(step_count)
-    if math.isclose(step_count, round(step_count)):  # 부동소수점 정밀도 해결
-        step_count = round(step_count)
-        foot_poses = pattern_generator.generate_crawl_gait_pattern(robot_speed, robot_step_hight, robot_motion)
-        motor_control(foot_poses, step_count)
+    def generate_foot_poses(self, speed):
+        return self.pattern_generator.generate_crawl_gait_pattern(speed, self.step_height, self.motion)
 
-    elif step_count ==0:
-        foot_poses = pattern_generator.generate_crawl_gait_pattern(robot_speed, robot_step_hight, robot_motion)
-        motor_control(foot_poses, 1000)
+    def motor_control(self, foot_poses, step_count, base_foot_poses):
+        num_steps = leg_resolution
+        motor_delay = 1 / (leg_resolution * 2)
 
-    else:
-        # 정수가 아닌 경우 처리
-        step_count_int = math.floor(step_count)
-        foot_poses = pattern_generator.generate_crawl_gait_pattern(robot_speed, robot_step_hight, robot_motion)
-        motor_control(foot_poses, step_count_int)
+        for _ in range(step_count):
+            for i in range(num_steps):
+                start_time = time.perf_counter()
 
-        remaining_distance = distance - (int(step_count) * robot_speed)
-        adjusted_speed = remaining_distance   # 나머지 거리만큼 속도 조정
-        foot_poses = pattern_generator.generate_crawl_gait_pattern(adjusted_speed, robot_step_hight, robot_motion)
-        motor_control(foot_poses, 1)
+                joint_angles = [
+                    self.inverse_kinematics.calculate_joint_angle(False, foot_poses[0][0][i] + base_foot_poses[0][0],
+                                                                  foot_poses[0][1][i] + base_foot_poses[0][1],
+                                                                  foot_poses[0][2][i] + base_foot_poses[0][2]),
+                    self.inverse_kinematics.calculate_joint_angle(True, foot_poses[1][0][i] + base_foot_poses[1][0],
+                                                                  foot_poses[1][1][i] + base_foot_poses[1][1],
+                                                                  foot_poses[1][2][i] + base_foot_poses[1][2]),
+                    self.inverse_kinematics.calculate_joint_angle(False, foot_poses[2][0][i] + base_foot_poses[2][0],
+                                                                  foot_poses[2][1][i] + base_foot_poses[2][1],
+                                                                  foot_poses[2][2][i] + base_foot_poses[2][2]),
+                    self.inverse_kinematics.calculate_joint_angle(True, foot_poses[3][0][i] + base_foot_poses[3][0],
+                                                                  foot_poses[3][1][i] + base_foot_poses[3][1],
+                                                                  foot_poses[3][2][i] + base_foot_poses[3][2]),
+                ]
 
-def motor_control(foot_pose,step_count):
+                angle_commands = [
+                    AngleCommand("fl_joint1", -joint_angles[0][0]),
+                    AngleCommand("fl_joint2", -joint_angles[0][1]),
+                    AngleCommand("fl_joint3", -2 * joint_angles[0][2]),
+                    AngleCommand("fr_joint1", joint_angles[1][0]),
+                    AngleCommand("fr_joint2", joint_angles[1][1]),
+                    AngleCommand("fr_joint3", 2 * joint_angles[1][2]),
+                    # Uncomment below lines for rear legs if needed
+                    # AngleCommand("rl_joint1", -joint_angles[2][0]),
+                    # AngleCommand("rl_joint2", -joint_angles[2][1]),
+                    # AngleCommand("rl_joint3", -2 * joint_angles[2][2]),
+                    # AngleCommand("rr_joint1", joint_angles[3][0]),
+                    # AngleCommand("rr_joint2", joint_angles[3][1]),
+                    # AngleCommand("rr_joint3", 2 * joint_angles[3][2]),
+                ]
 
-    base_foot_poses = inverse_kinematics.calculate_foot_position_with_orientation(*robot_orientaion)
-    num_steps = leg_resolution
+                self.motor_controller.move_motors(angle_commands)
 
-    for i in range(step_count):
-        for i in range(num_steps):
+                while time.perf_counter() - start_time < motor_delay:
+                    pass
 
-            start_time = time.perf_counter()
+        # Reset to base foot poses
+        self.reset_to_base_pose(base_foot_poses)
 
-            fl_theta1, fl_theta2, fl_theta3 = inverse_kinematics.calculate_joint_angle(False, foot_pose[0][0][i]+base_foot_poses[0][0], foot_pose[0][1][i]+base_foot_poses[0][1], foot_pose[0][2][i]+base_foot_poses[0][2])
-            fr_theta1, fr_theta2, fr_theta3 = inverse_kinematics.calculate_joint_angle(True, foot_pose[1][0][i]+base_foot_poses[1][0], foot_pose[1][1][i]+base_foot_poses[1][1], foot_pose[1][2][i]+base_foot_poses[1][2])
-            rl_theta1, rl_theta2, rl_theta3 = inverse_kinematics.calculate_joint_angle(False, foot_pose[2][0][i]+base_foot_poses[2][0], foot_pose[2][1][i]+base_foot_poses[2][1], foot_pose[2][2][i]+base_foot_poses[2][2])
-            rr_theta1, rr_theta2, rr_theta3 = inverse_kinematics.calculate_joint_angle(True, foot_pose[3][0][i]+base_foot_poses[3][0], foot_pose[3][1][i]+base_foot_poses[3][1], foot_pose[3][2][i]+base_foot_poses[3][2])
+    def reset_to_base_pose(self, base_foot_poses):
+        joint_angles = [
+            self.inverse_kinematics.calculate_joint_angle(False, *base_foot_poses[0]),
+            self.inverse_kinematics.calculate_joint_angle(True, *base_foot_poses[3]),
+        ]
 
-            # print(" left_x: ",foot_pose[0][0][i]+base_foot_poses[0][0]," left_y: ",foot_pose[0][1][i]+base_foot_poses[0][1] ," left_z: ",foot_pose[0][2][i]+base_foot_poses[0][2] )
-            # print(" right_x: ",foot_pose[1][0][i]+base_foot_poses[1][0]," right_y: ",foot_pose[1][1][i]+base_foot_poses[1][1] ," right_z: ",foot_pose[1][2][i]+base_foot_poses[1][2] )
-            angle_commands = [
-                AngleCommand("fl_joint1", -fl_theta1),
-                AngleCommand("fl_joint2", -fl_theta2),
-                AngleCommand("fl_joint3", -2 * (fl_theta3)),
-                AngleCommand("fr_joint1", fr_theta1),
-                AngleCommand("fr_joint2", fr_theta2),
-                AngleCommand("fr_joint3", 2 * (fr_theta3)),
-                # AngleCommand("rl_joint1", rl_theta1),
-                # AngleCommand("rl_joint2", rl_theta2),
-                # AngleCommand("rl_joint3", 2 * (rl_theta3)),
-                # AngleCommand("rr_joint1", -rr_theta1),
-                # AngleCommand("rr_joint2", -rr_theta2),
-                # AngleCommand("rr_joint3", -2 *(rr_theta3)),
-            ]
-#
-#             # 모터 명령 동기적으로 실행
-            motor_controller.move_motors(angle_commands)
-#             # 약간의 지연 추가
-            while time.perf_counter() - start_time < motor_delay:
-                pass  # 원하는 시간만큼 대기
+        angle_commands = [
+            AngleCommand("fl_joint1", -joint_angles[0][0]),
+            AngleCommand("fl_joint2", -joint_angles[0][1]),
+            AngleCommand("fl_joint3", -2 * joint_angles[0][2]),
+            AngleCommand("rr_joint1", joint_angles[1][0]),
+            AngleCommand("rr_joint2", joint_angles[1][1]),
+            AngleCommand("rr_joint3", 2 * joint_angles[1][2]),
+        ]
 
-    motor_controller.close()
+        self.motor_controller.move_motors(angle_commands)
+        self.motor_controller.close()
+
+    def drive(self):
+        base_foot_poses = self.inverse_kinematics.calculate_foot_position_with_orientation(*self.orientation)
+        step_count = self.calculate_steps()
+
+        if math.isclose(step_count, round(step_count)):
+            step_count = round(step_count)
+            foot_poses = self.generate_foot_poses(self.robot_speed)
+            self.motor_control(foot_poses, step_count, base_foot_poses)
+        elif step_count == 0:
+            foot_poses = self.generate_foot_poses(self.robot_speed)
+            self.motor_control(foot_poses, 1000, base_foot_poses)
+        else:
+            step_count_int = math.floor(step_count)
+            foot_poses = self.generate_foot_poses(self.robot_speed)
+            self.motor_control(foot_poses, step_count_int, base_foot_poses)
+
+            remaining_distance = self.distance - (step_count_int * self.robot_speed)
+            adjusted_speed = remaining_distance
+            foot_poses = self.generate_foot_poses(adjusted_speed)
+            self.motor_control(foot_poses, 1, base_foot_poses)
+
 
 if __name__ == "__main__":
+    controller = RobotController(
+        robot_speed=120,
+        distance=2400,
+        step_height=50,
+        motion="forward",
+        orientation=[0, 0, 0],
+    )
     time.sleep(2)
-    driving_contoller()
+    controller.drive()
