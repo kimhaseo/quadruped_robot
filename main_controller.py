2c0fd3c2a4ce08134177a890377d274951739ace
kimhaseo <gktj0698@gmail.com>
2024-12-15 17:30:40 +0900

    제어 로직 수정

--- main_controller.py
+++ main_controller.py
@@ -1,33 +1,30 @@
 import sys
 import os
 import time
-from config.pose_cmd import PoseCommand
 from solver.stabilizer import StabilizerSolver
 from controller.motion_controller import MotionController
 from config.config import init_pose
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-stabillizer_controller = StabilizerSolver()
+stabilizer_controller = StabilizerSolver()
 motion_controller = MotionController()
-taget_orientation = [0,10,0]
+target_orientation = [0,10,0]
 
 def main():
 
-    # target_foot_coord = stabillizer_controller.stabilize(taget_orientation)
+    # target_foot_coord = stabilizer_controller.stabilize(target_orientation)
     motion_controller.pose_control(init_pose)
     time.sleep(5)
-    print("자세교정1")
-    target_foot_coord = stabillizer_controller.stabilize(taget_orientation)
+    target_foot_coord = stabilizer_controller.stabilize(target_orientation)
     motion_controller.pose_control(target_foot_coord)
     time.sleep(5)
-    print("자세교정2")
     motion_controller.move_contorl(30,20,50,"forward")
     time.sleep(5)
 
     # while True:
     #
-    #     target_foot_coord = stabillizer_controller.stabilize(taget_orientation)
+    #     target_foot_coord = stabilizer_controller.stabilize(target_orientation)
     #     motion_controller.pose_control(target_foot_coord)
         # print(target_foot_coord)
 
