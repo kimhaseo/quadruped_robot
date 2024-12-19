fef223d4f6e762b70534dfb1043f2153a39f0c0e
kimhaseo <gktj0698@gmail.com>
2024-12-19 22:41:11 +0900

    균형 유지 개발

--- solver/stabilizer.py
+++ solver/stabilizer.py
@@ -5,9 +5,11 @@ from ctypes.wintypes import tagRECT
 import numpy as np
 import threading
 import time
+
+from numpy.f2py.crackfortran import kindselector
+
 from manager.pose_manager import pose_cmd
 from sensor.ahrs import MW_AHRS
-from fontTools.misc.arrayTools import offsetRect
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from solver.inverse import Kinematics
 
@@ -28,29 +30,19 @@ class StabilizerSolver:
             current_orientation = body_pose["body_orientation"]
             diff_orientation = np.array(target_orientation) - (np.array(current_orientation))
 
-            if any(30 > value > 1 for value in diff_orientation):
-                pass
+            if any(15 > abs(value) > 1 for value in diff_orientation):
 
-            elif any(value >= 20 for value in diff_orientation):
+                calibartion_orientation = np.array(target_orientation) + np.array(diff_orientation)
+                coords = self.kinematics.calculate_foot_position_with_orientation(*calibartion_orientation)
+                print(coords)
+
+            elif any(abs(value) >= 15 for value in diff_orientation):
                 raise ValueError ("자세 이상")
 
             else:
-                diff_orientation = [0,0,0]
+                pass
 
-            calibration_orientation = np.array(target_orientation) + (np.array(diff_orientation))
-            coords = self.kinematics.calculate_foot_position_with_orientation(*calibration_orientation)
-
-            feet_positions = {
-                "fl_foot": coords["fl_foot"],
-                "fr_foot": coords["fr_foot"],
-                "rl_foot": coords["rl_foot"],
-                "rr_foot": coords["rr_foot"],
-            }
-            for foot, position in feet_positions.items():
-                pose_cmd.update_pose(foot, position)
-            time.sleep(0.1)
-
-            print(pose_cmd.get_pose())
+            print(diff_orientation)
 
 if __name__ == "__main__":
 
